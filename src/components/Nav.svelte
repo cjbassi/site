463dc6d6b20eb24825dd9a445c24e70e83438ed1
Caleb Bassi <calebjbassi@gmail.com>
2019-10-17 15:37:30 -0700

    Cleanup colorscheme code

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,32 +1,32 @@
 <script>
 	import { onMount } from 'svelte';
 
-	let colorscheme = "dark";
+	let colorScheme = "dark";
 
 	onMount(() => {
-		colorscheme = getColorscheme();
+		colorScheme = getColorScheme();
 	});
 
-	function getColorscheme() {
+	function getColorScheme() {
 		return document.body.classList.contains("solarized-dark") ? "dark" : "light";
 	}
 
-	function oppositeColorscheme(s) {
+	function oppositeColorScheme(s) {
 		if (typeof s !== 'string') return '';
 		return s === "light" ? "dark" : "light";
 	}
 
-	function toggleColorscheme() {
-		if (colorscheme === "light") {
+	function toggleColorScheme() {
+		if (colorScheme === "light") {
 			document.body.classList.remove("solarized-light");
 			document.body.classList.add("solarized-dark");
-			localStorage.setItem("colorscheme", "dark");
+			localStorage.setItem("color-scheme", "dark");
 		} else {
 			document.body.classList.remove("solarized-dark");
 			document.body.classList.add("solarized-light");
-			localStorage.setItem("colorscheme", "light");
+			localStorage.setItem("color-scheme", "light");
 		}
-		colorscheme = oppositeColorscheme(colorscheme);
+		colorScheme = oppositeColorScheme(colorScheme);
 	}
 
 	function capitalize(s) {
@@ -48,8 +48,8 @@
 		-
 		<a rel="prefetch" href="blog">Blog</a>
 		|
-		<a href="javascript:void(0)" on:click={toggleColorscheme}>
-			{capitalize(oppositeColorscheme(colorscheme))} mode
+		<a href="javascript:void(0)" on:click={toggleColorScheme}>
+			{capitalize(oppositeColorScheme(colorScheme))} mode
 		</a>
 	</p>
 </nav>
